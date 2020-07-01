b1ba4cab33b3ffcd66af00937422b9c30c225597
Po-Kang <pkang@changanus.com>
2020-07-01 17:32:36 -0400

    opencv finished

--- Udacity_Camera/OpenCV_exercises/src/create_matrix.cpp
+++ Udacity_Camera/OpenCV_exercises/src/create_matrix.cpp
@@ -16,7 +16,9 @@ void createMatrix1()
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
-
+    cv::Mat m3_8u;
+    m3_8u.create(nrows,ncols,CV_8UC3);
+    m3_8u.setTo(cv::Scalar(255));
 
     // show result
     string windowName = "First steps in OpenCV (m1_8u)";
@@ -26,7 +28,10 @@ void createMatrix1()
 
     // STUDENT TASK :
     // Display the results from the STUDENT TASK above
-
+    string windowName1 = "First steps in OpenCV (m3_8u)";
+    cv::namedWindow(windowName1, 2); // create window
+    cv::imshow(windowName1, m3_8u);
+    cv::waitKey(0); // wait for keyboard input before continuing
 }
 
 
